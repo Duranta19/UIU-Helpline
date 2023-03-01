9ceee4bfa3e5191433a0d094275afa8ef1063560
sayemsiam <sayemsiam198@gmail.com>
2023-03-01 16:11:42 +0600

    community front-end without modal

--- community.php
+++ community.php
@@ -2,6 +2,35 @@
 $title = "Community";
 include "components/header.php";
 ?>
+<div class="container">
+    <div class="row justify-content-md-end">
+        <button type="button" class="btn my-3 p-0" style="width:130px; height:40px; background-color: #15252B; border-radius: 15px; color:white;"><b>Post</b></button>
+    </div>
+</div>
+<hr>
+
+<div class="card w-75 m-auto">
+  <div class="card-header" style="display:inline-flex">
+    <img src="https://t3.ftcdn.net/jpg/05/34/22/36/360_F_534223627_0JFVJDBwNku7LyLazrtN6YBTJ2agUfP5.jpg" alt="" style="width:50px; height:50px; border-radius:50%; margin-top:2px">
+    <p style="padding: 8px 10px; font-size:20px"><b>User Name</b></p>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title"><a href="">Post Title</a></h5>
+    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae debitis aliquid nemo nobis expedita sapiente nostrum iusto libero quae quia officiis accusantium aperiam dicta illo unde, aspernatur molestiae? Tempora, itaque.</p>
+
+  </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 <br>
 <br>
 <?php
--- components/header.php
+++ components/header.php
@@ -5,17 +5,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <title><?php echo $title ?></title>
     <style>
-        .hov:hover{
+        .hov:hover {
             font-weight: 200;
             background-color: rgb(28, 56, 56);
         }
-        footer div p{
+
+        footer {
+            width: 100%;
+            display: block;
+            position: static;
+        }
+
+        footer div p {
             margin-top: -22px;
+        }
+
+        a {
+            color: #141619;
+            text-decoration: underline;
         }
     </style>
 </head>
@@ -26,13 +37,10 @@
         <nav class="navbar navbar-expand-lg" style="background-color: #15252B;">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">
-                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/United_International_University_Monogram.svg/1200px-United_International_University_Monogram.svg.png"
-                        alt="" width="35" height="30">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/United_International_University_Monogram.svg/1200px-United_International_University_Monogram.svg.png" alt="" width="35" height="30">
                 </a>
                 <a class="navbar-brand" href="#" style="color: white;"><b>UIU Helpline</b></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -64,4 +72,3 @@
             </div>
         </nav>
     </div>
-    
