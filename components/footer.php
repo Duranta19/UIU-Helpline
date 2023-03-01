cfb91c5ec4d814af847e8adad08537b3df5abb5a
sayemsiam <sayemsiam198@gmail.com>
2023-03-01 17:19:01 +0600

    components

---
+++ components/footer.php
@@ -0,0 +1,16 @@
+
+    <footer>
+            <p style="text-align: center; color: white;"><b>UIU Helpline</b></p>
+            <p style="text-align: center; color: white;">www.uiuhelpline.com</p>
+            <p style="text-align: center; color: white;">© ALL RIGHTS RESERVE BY TEAM VOID</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
